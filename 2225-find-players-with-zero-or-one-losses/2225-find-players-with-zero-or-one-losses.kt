03dead61be6e520aebe95d2838dbf57d55e8e804
Sage <46391642+ssaje@users.noreply.github.com>
2024-01-15 23:59:38 +0900

    Time: 1421 ms (17.24%), Space: 123.8 MB (5.17%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.kt
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.kt
@@ -1,9 +1,9 @@
 class Solution {
     fun findWinners(matches: Array<IntArray>): List<List<Int>> {
-        val indeg = mutableMapOf<Int, Int>()
+        val indeg = HashMap<Int, Int>()
         for ((winner, loser) in matches) {
-            indeg[winner] = indeg[winner] ?: 0
-            indeg[loser] = (indeg[loser] ?: 0) + 1
+            indeg.putIfAbsent(winner, 0)
+            indeg[loser] = indeg.getOrPut(loser) { 0 } + 1
         }
 
         return listOf(
