d22fddd5b1c35294e9116a0c050bdabb9ee865dc
Sage <46391642+ssaje@users.noreply.github.com>
2024-02-04 13:49:07 +0900

    Time: 200 ms (93.16%), Space: 37.8 MB (88.59%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.kt
@@ -0,0 +1,40 @@
+class Solution {
+    fun minWindow(s: String, t: String): String {
+        val counts = IntArray('z' - 'A' + 1)
+        var totalCount = 0
+        for (ch in t) {
+            val count = counts[ch - 'A']++
+            if (count == 0) {
+                totalCount++
+            }
+        }
+
+        var minCount = Int.MAX_VALUE
+        var answer = 0 to -1
+        var start = 0
+        for (end in s.indices) {
+            val count = --counts[s[end] - 'A']
+            if (count == 0) {
+                --totalCount
+            }
+
+            while (start < end) {
+                val index = s[start] - 'A'
+                val count = counts[index]
+                if (count >= 0) {
+                    break
+                }
+
+                counts[index]++
+                start++
+            }
+
+            if (totalCount == 0 && minCount > end - start) {
+                minCount = end - start
+                answer = start to end
+            }
+        }
+
+        return s.slice(answer.first..answer.second)
+    }
+}
