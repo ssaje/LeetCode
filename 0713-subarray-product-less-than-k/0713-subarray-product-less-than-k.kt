555dfebe884892256c6bea6157063426c30708da
Sage <46391642+ssaje@users.noreply.github.com>
2024-03-27 20:46:35 +0900

    Time: 304 ms (55.17%), Space: 43.4 MB (10.35%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.kt
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.kt
@@ -1,21 +1,18 @@
 class Solution {
     fun numSubarrayProductLessThanK(nums: IntArray, k: Int): Int {
-        var count = 0
         var product = 1L
         var s = 0
         var answer = 0
         for (e in nums.indices) {
             product *= nums[e]
-            count++
 
             while (product >= k && s <= e) {
                 product /= nums[s]
                 s++
-                count--
             }
 
             if (product < k) {
-                answer += count
+                answer += e - s + 1
             }
         }
 
