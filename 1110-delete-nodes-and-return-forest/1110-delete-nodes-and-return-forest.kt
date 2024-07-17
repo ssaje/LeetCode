8c0187ba7964053b9d60161aa38816cd0a2987cc
Sage <46391642+ssaje@users.noreply.github.com>
2024-07-17 22:46:11 +0900

    Time: 277 ms (5.88%), Space: 45.7 MB (7.35%) - LeetHub

--- 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.kt
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.kt
@@ -11,27 +11,24 @@
 class Solution {
     fun delNodes(root: TreeNode?, to_delete: IntArray): List<TreeNode?> {
         val trees = mutableListOf<TreeNode>()
-        val valsToBeDeleted = to_delete.toSet()
+        val valsToBeDeleted = to_delete.toHashSet()
 
-        fun dfs(node: TreeNode?, insert: Boolean): Boolean {
+        fun dfs(node: TreeNode?, insert: Boolean): TreeNode? {
             if (node == null) {
-                return false
+                return node
             }
-            
+
             val isDeleted = node.`val` in valsToBeDeleted
-            if (!isDeleted && insert) {
+            node.left = dfs(node.left, isDeleted)
+            node.right = dfs(node.right, isDeleted)
+
+            if (isDeleted) {
+                return null
+            } else if (insert) {
                 trees += node
             }
 
-            if (dfs(node.left, isDeleted)) {
-                node.left = null
-            }
-            
-            if (dfs(node.right, isDeleted)) {
-                node.right = null
-            }
-
-            return isDeleted
+            return node
         }
 
         dfs(root, true)
