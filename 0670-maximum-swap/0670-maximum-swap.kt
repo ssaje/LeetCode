e78a3585dc9dc2e98562f5b5a9544a873f37ca4a
Sage <46391642+ssaje@users.noreply.github.com>
2024-10-17 22:13:02 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0670-maximum-swap/0670-maximum-swap.kt
@@ -0,0 +1,25 @@
+class Solution {
+    fun maximumSwap(num: Int): Int {
+        val chars = num.toString().toCharArray()
+        val max = chars.max()
+        for (i in chars.indices) {    
+            if (chars[i] == max) {
+                continue
+            }
+
+            var index = i
+            for (j in i+1..chars.lastIndex) {
+                if (chars[index] <= chars[j]) {
+                    index = j
+                }
+            }
+
+            if (index != i) {
+                chars[i] = chars[index].also { chars[index] = chars[i] }
+                return String(chars).toInt()
+            }
+        }
+
+        return num
+    }
+}
