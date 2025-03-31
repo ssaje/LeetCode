08a0d129f7373a47e38ed9ba165e8c1ad2343f39
Sage <46391642+ssaje@users.noreply.github.com>
2025-03-31 22:53:23 +0900

    Time: 236 ms (0%), Space: 63.9 MB (75%) - LeetHub

--- 2551-put-marbles-in-bags/2551-put-marbles-in-bags.kt
+++ 2551-put-marbles-in-bags/2551-put-marbles-in-bags.kt
@@ -1,9 +1,10 @@
 class Solution {
     fun putMarbles(weights: IntArray, k: Int): Long {
-        val sums = IntArray(weights.size - 1) { weights[it] + weights[it + 1] }.sorted()
+        val n = weights.size
+        val sums = IntArray(n - 1) { weights[it] + weights[it + 1] }.sorted()
         var answer = 0L
         for (i in 0..<k-1) {
-            answer += sums[weights.size - 2 - i] - sums[i]
+            answer += sums[n - 2 - i] - sums[i]
         }
 
         return answer
