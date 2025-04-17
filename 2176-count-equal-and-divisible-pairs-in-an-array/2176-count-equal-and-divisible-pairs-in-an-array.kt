554cd99b23ee2a3962815f7f068f5ad71893780a
Sage <46391642+ssaje@users.noreply.github.com>
2025-04-17 22:14:25 +0900

    Time: 9 ms (20%), Space: 44.8 MB (0%) - LeetHub

--- 2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.kt
+++ 2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.kt
@@ -1,7 +1,7 @@
 class Solution {
     fun countPairs(nums: IntArray, k: Int): Int {
         var answer = 0
-        for (i in nums.indices) {
+        for (i in 0..<nums.size-1) {
             for (j in i+1..<nums.size) {
                 if (nums[i] == nums[j] && i * j % k == 0) {
                     answer++
