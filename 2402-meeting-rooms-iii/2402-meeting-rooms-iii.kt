03e5e26f8856ab93eac016a18dfa0dcaf57bd160
Sage <46391642+ssaje@users.noreply.github.com>
2025-07-12 00:49:52 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2402-meeting-rooms-iii/2402-meeting-rooms-iii.kt
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.kt
@@ -1,41 +1,44 @@
 class Solution {
     fun mostBooked(n: Int, meetings: Array<IntArray>): Int {
-        val counts = IntArray(n)
-        val rooms = PriorityQueue<Int>()
-        val openings = PriorityQueue<Pair<Long, Int>> { a, b ->
-            if (a.first != b.first) {
-                a.first.compareTo(b.first)
-            } else {
-                a.second - b.second
-            }
+        // room, startTime
+        val emptyRooms = PriorityQueue<Pair<Int, Int>> { a, b -> a.first - b.first }
+        for (i in 0..<n) {
+            emptyRooms.offer(i to 0)
         }
-
-        repeat(n) { rooms.add(it) }
 
         val sorted = meetings.sortedBy { it[0] }
-        var max = 0
-        for ((start, end) in sorted) {
-            while (openings.isNotEmpty()) {
-                val opening = openings.peek()
-                if (start < opening.first) {
-                    break
-                }
-
-                rooms.add(opening.second)
-                openings.remove()
+        // endTime, room
+        val ongoings = PriorityQueue<Pair<Int, Int>> { a, b ->
+            if (a.first != b.first) a.first - b.first
+            else a.second - b.second
+        }
+        val counts = IntArray(n)
+        for (meeting in sorted) {
+            while (ongoings.isNotEmpty() && ongoings.peek().first <= meeting[0]) {
+                val (endTime, room) = ongoings.poll()
+                emptyRooms.offer(room to endTime)
             }
 
-            if (rooms.isEmpty()) {
-                val (t, room) = openings.remove()
-                openings.add(t + end - start to room)
-                max = max.coerceAtLeast(++counts[room])
-            } else {
-                val room = rooms.remove()
-                openings.add(end.toLong() to room)
-                max = max.coerceAtLeast(++counts[room])
+            if (emptyRooms.isEmpty()) {
+                val (endTime, room) = ongoings.poll()
+                emptyRooms.offer(room to endTime)
+            }
+
+            val (room, startTime) = emptyRooms.poll()
+            counts[room]++
+            val endTime = maxOf(meeting[1], startTime + meeting[1] - meeting[0])
+            ongoings.offer(endTime to room)
+        }
+
+        var answer = 0
+        var max = 0
+        for (i in 0..<n) {
+            if (max < counts[i]) {
+                max = counts[i]
+                answer = i
             }
         }
 
-        return counts.indexOfFirst { it == max }
+        return answer
     }
 }
