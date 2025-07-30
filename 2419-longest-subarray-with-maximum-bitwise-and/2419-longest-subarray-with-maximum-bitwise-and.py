a7c10ffc9747946d58633fd3d501d79e187569f1
Sage <46391642+ssaje@users.noreply.github.com>
2025-07-30 19:08:04 +0900

    Time: 24 ms (87.57%), Space: 31.6 MB (5.92%) - LeetHub

--- 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.py
+++ 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.py
@@ -1,12 +1,8 @@
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
         m = max(nums)
-        l = -1
-        answer = 0
-        for r, num in enumerate(nums):
-            if num != m:
-                l = r
-            else:
-                answer = max(answer, r - l)
-
-        return answer
+        return max(
+            len(list(group))
+            for key, group in groupby(nums)
+            if key == m
+        )
