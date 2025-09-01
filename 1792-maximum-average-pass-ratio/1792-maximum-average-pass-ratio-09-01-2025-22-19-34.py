6a999e9e8780ec342b406c400ccff4721411fe39
Sage <46391642+ssaje@users.noreply.github.com>
2025-09-01 22:19:36 +0900

    Time: 1049 ms (93.48%), Space: 55 MB (46.96%) - LeetHub

---
+++ 1792-maximum-average-pass-ratio/1792-maximum-average-pass-ratio-09-01-2025-22-19-34.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxAverageRatio(self, classes: List[List[int]], extraStudents: int) -> float:
+        heap = [(p / t - (p + 1) / (t + 1), p, t) for p, t in classes]
+        heapify(heap)
+
+        for _ in range(extraStudents):
+            _, p, t = heappop(heap)
+            heappush(heap, ((p + 1) / (t + 1) - (p + 2) / (t + 2), p + 1, t + 1))
+
+        return sum(p / t for _, p, t in heap) / len(classes)
