f62d85415373bb42419af3f66c4ea3ef957a481d
Sage <46391642+ssaje@users.noreply.github.com>
2025-11-28 22:42:42 +0900

    Time: 95 ms (96.38%), Space: 39.9 MB (45.65%) - LeetHub

---
+++ 2872-maximum-number-of-k-divisible-components/2872-maximum-number-of-k-divisible-components-11-28-2025-22-42-44.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maxKDivisibleComponents(self, n: int, edges: List[List[int]], values: List[int], k: int) -> int:
+        g = [[] for _ in range(n)]
+        for a, b in edges:
+            g[a].append(b)
+            g[b].append(a)
+
+        answer = 0
+        def dfs(u: int, p: int) -> int:
+            nonlocal answer
+            
+            sum = values[u] % k
+            for v in g[u]:
+                if v == p:
+                    continue
+                
+                sum = (sum + dfs(v, u)) % k
+
+            if sum == 0:
+                answer += 1
+
+            return sum
+
+        dfs(0, -1)
+
+        return answer
