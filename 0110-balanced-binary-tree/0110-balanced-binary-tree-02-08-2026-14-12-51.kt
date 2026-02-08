e80558e02c20dd570b050acf75f1f2aab8695a8f
Sage <46391642+ssaje@users.noreply.github.com>
2026-02-08 14:12:52 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree-02-08-2026-14-12-51.kt
@@ -0,0 +1,26 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun isBalanced(root: TreeNode?): Boolean {
+        if (root == null) return true
+
+        val (hl, hr) = height(root)
+        return hl == hr || hl + 1 == hr || hl == hr + 1
+    }
+
+    private fun height(node: TreeNode): Pair<Int, Int> {
+        val hl = node.left?.let { height(it).max() + 1 } ?: 0
+        val hr = node.right?.let { height(it).max() + 1 } ?: 0
+        return hl to hr
+    }
+
+    private fun Pair<Int, Int>.max(): Int = maxOf(first, second)
+}
