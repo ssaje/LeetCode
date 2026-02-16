249d46581bf75f627e530fe8e2a4082c44f54f66
Sage <46391642+ssaje@users.noreply.github.com>
2026-02-16 18:14:45 +0900

    Time: 76 ms (98.03%), Space: 39.7 MB (69.74%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits-02-16-2026-18-14-46.kt
@@ -0,0 +1,12 @@
+class Solution {
+    fun reverseBits(n: Int): Int {
+        var answer = 0
+        var m = n
+        for (i in 0..<32) {
+            answer = (answer shl 1) or (m and 1)
+            m = m shr 1
+        }
+
+        return answer 
+    }
+}
